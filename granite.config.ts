3ccbad81bb8aa630b72e077048e7870a6b871e72
hyun522 <jhj1004v@naver.com>
2026-07-07 20:37:36 +0900

    SETTING : camera/photos/clipboard 권한 추가

--- granite.config.ts
+++ granite.config.ts
@@ -3,18 +3,35 @@ import { defineConfig } from "@apps-in-toss/web-framework/config";
 export default defineConfig({
   appName: "worry-off",
   brand: {
-    displayName: "걱정 OFF", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+    displayName: "걱정 끄기", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#0863F4", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
     icon: "/logo.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
-    host: "localhost",
+    host: "0.0.0.0",
     port: 5173,
     commands: {
-      dev: "vite dev",
+      dev: "vite --host",
       build: "vite build",
     },
   },
-  permissions: [],
+  permissions: [
+    {
+      name: "clipboard",
+      access: "read",
+    },
+    {
+      name: "clipboard",
+      access: "write",
+    },
+    {
+      name: "camera",
+      access: "access",
+    },
+    {
+      name: "photos",
+      access: "read",
+    },
+  ],
   outdir: "dist",
 });
