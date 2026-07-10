a840462a7ea2445bab7516d92f51f5ed9a87dfda
hyun522 <jhj1004v@naver.com>
2026-07-10 12:06:49 +0900

    CHORE : logo 이미지 주소 변경

--- granite.config.ts
+++ granite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   brand: {
     displayName: "걱정 끄기", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#0863F4", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "/logo.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: "https://static.toss.im/appsintoss/55295/87f2775a-6df6-4063-80a6-9eceb56d5667.png", // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
     host: "0.0.0.0",
